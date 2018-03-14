db1078a6269b845badc711f7bad2cf13eb32b1b6
Saned Company <saned.inc@gmail.com>
2018-03-14 16:56:31 +0200

    search in orders

--- app/Http/Controllers/Admin/OrdersController.php
+++ app/Http/Controllers/Admin/OrdersController.php
@@ -32,17 +32,17 @@ class OrdersController extends Controller
         $orders = [] ;
         if ($request->service_type != '' && $request->service_provider == '') {
             
-            $serviceType = Category::where('name_ar','like',$request->service_type)->first();
+            $serviceType = Category::where('name_ar','like','%'.$request->service_type.'%')->first();
             if($serviceType){
                 $orders = Order::join('companies','orders.company_id','companies.id')->join('users','orders.user_id','users.id')->join('services','orders.service_id','services.id')->where('companies.category_id',$serviceType->id)->select('orders.*','companies.id as company_id' , 'companies.name as company_name' , 'companies.category_id as serviceType' ,'companies.place as company_place' , 'companies.user_id as provider_id','companies.city_id as city_id','users.id as user_id' , 'users.name as username' , 'services.id as service_id' , 'services.name as service_name', 'services.description as service_desc')->get(); 
             }
 
         }elseif ($request->service_type == '' && $request->service_provider != '') {
             
-            $serviceProvider = User::where('name','like',$request->service_provider)->first();
+            $serviceProvider = User::where('name','like','%'.$request->service_provider.'%')->first();
             
             if($serviceProvider){
-                $orders = Order::join('companies','orders.company_id','companies.id')->join('users','orders.user_id','users.id')->join('services','orders.service_id','services.id')->where('companies.user_id',$serviceProvider->id . '%')->select('orders.*','companies.id as company_id' , 'companies.name as company_name' , 'companies.category_id as serviceType' ,'companies.place as company_place' , 'companies.user_id as provider_id','companies.city_id as city_id','users.id as user_id' , 'users.name as username' , 'services.id as service_id' , 'services.name as service_name', 'services.description as service_desc')->get(); 
+                $orders = Order::join('companies','orders.company_id','companies.id')->join('users','orders.user_id','users.id')->join('services','orders.service_id','services.id')->where('companies.user_id',$serviceProvider->id)->select('orders.*','companies.id as company_id' , 'companies.name as company_name' , 'companies.category_id as serviceType' ,'companies.place as company_place' , 'companies.user_id as provider_id','companies.city_id as city_id','users.id as user_id' , 'users.name as username' , 'services.id as service_id' , 'services.name as service_name', 'services.description as service_desc')->get(); 
             }
 
         }elseif ($request->service_type != '' && $request->service_provider != '') {
